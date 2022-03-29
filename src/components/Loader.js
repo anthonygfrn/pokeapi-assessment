76666729d9ca2afdaef469bb6f7886d6eac85d56
Anthony Gufron <anthony.gufron@kumparan.com>
2022-03-29 09:36:43 +0700

    Add Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { Spinner } from "../styles/components/Spinner";
+
+export const Loader = () => {
+    return (
+        <div css={Spinner}>
+            <div class="lds-facebook"><div></div><div></div><div></div></div>
+        </div>
+    )
+}
